1880ee5a617aea5ccea349e384f24d1428225070
Barhan Akmal Falahudin <67768493+akmalf06@users.noreply.github.com>
2021-02-14 10:43:24 +0700

    add celery

--- backend/tasks.py
+++ backend/tasks.py
@@ -3,9 +3,10 @@ import time
 from celery import shared_task
 
 @shared_task
-def laporkan_pusat(instance):
-    time.sleep(10000)
-    instance.lapor_pusat = True
-    instance.save()
+def laporkan_pusat(pk):
+    time.sleep(10)
+    laporan = Laporan.objects.get(pk=pk)
+    laporan.lapor_pusat = True
+    laporan.save()
 
-    return None
+    return 'masuk sini'
